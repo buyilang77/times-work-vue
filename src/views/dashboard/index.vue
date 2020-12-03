2239a04ee0b05d744d65486062e5e941ce9d05f1
Ethan Ke <zhennankee@gmail.com>
2020-12-03 19:41:40 +0800

    Update statistics

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,20 +1,9 @@
 <template>
   <div class="dashboard-container">
     <el-table :data="statistics" border style="width: 100%">
-      <el-table-column prop="date" label="日期" width="180" />
-      <el-table-column prop="zhangying" label="张颖" />
-      <el-table-column prop="maruiqin" label="马瑞琴" />
-      <el-table-column prop="dupengfei" label="杜鹏飞" />
-      <el-table-column prop="liupengtao" label="刘朋涛" />
-      <el-table-column prop="zuoyue" label="左悦" />
-      <el-table-column prop="zhugang" label="朱刚" />
-      <el-table-column prop="yangyang" label="杨洋" />
-      <el-table-column prop="zhangjinxia" label="张金侠" />
-      <el-table-column prop="zhangwen" label="张文" />
-      <el-table-column prop="liujia" label="刘佳" />
-      <el-table-column prop="wangling" label="王玲" />
-      <el-table-column prop="liujiangxia" label="刘江霞" />
-      <el-table-column prop="wuming" label="武明" />
+      <el-table-column prop="created_at" label="日期" width="180" />
+      <el-table-column prop="sum" label="总计" />
+      <el-table-column prop="name" label="姓名" />
     </el-table>
   </div>
 </template>
